db7ff76a1ab384f5d33163b32964fc94c4d57d60
MihirVir <mihirvir08@gmail.com>
2023-02-27 20:19:19 +0530

    cors removed

--- index.js
+++ index.js
@@ -19,10 +19,7 @@ require('dotenv').config();
 // middlewares
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors({
-    origin: "*",
-    credentials: true
-}))
+app.use(cors());
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'uploads')))
 app.use(express.static(path.join(__dirname, 'templates')))
