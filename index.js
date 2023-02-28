4d949ef4ebbe7ab195cc16c17b95c65464f67376
MihirVir <mihirvir08@gmail.com>
2023-02-28 12:03:45 +0530

    added local host

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ require('dotenv').config();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors({
-    origin: ["https://www.mihir-major-project.netlify.app/", "https://www.mihir-major-project-course.netlify.app/"],
+    origin: ["http://localhost:3000/", "https://www.mihir-major-project.netlify.app/", "https://www.mihir-major-project-course.netlify.app/"],
     methods: ["PUT", "GET", "POST", "DELETE", "PATCH"]
 }));
 app.use(cookieParser())
