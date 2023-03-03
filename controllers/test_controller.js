7657e2de7077dba6f242f7e20c163faf9fc40089
MihirVir <mihirvir08@gmail.com>
2023-03-03 18:37:57 +0530

    made response time a bit better

--- controllers/test_controller.js
+++ controllers/test_controller.js
@@ -1,7 +1,7 @@
 
 const Test = require('../models/test');
 const Video = require('../models/video');
-
+const Purchased = require('../models/purchased');
 const createTestCourse = async (req, res) => {
     try {
         const userId = req.user.id;
@@ -43,13 +43,91 @@ const deleteTestCourse = async (req, res) => {
     try {
         const courseId = req.params.id;
         const userId = req.user.id;
-        const isCourse = await Test.findOneAndDelete({_id: courseId, $match: { author: userId }})
+        const isCourse = await Test.findOneAndDelete({_id: courseId,  author: userId })
 
         const deleteVideos = await Video.deleteMany({courseId: courseId});
         return res
                 .status(200)
                 .json(successObj = {isCourse, deleteVideos,message: "Successfully deleted the course"})
     } catch (err) {
+        console.log(err);
+        return res
+                .status(500)
+                .json({
+                    message: "Internal Server Error"
+                })
+    }
+}
+
+const getSpecificCourse = async (req, res) => {
+    try {
+        const courseId = req.params.id;
+        const userId = req.user.id;
+        // searching course 
+        const isCourse = await Test.findById(courseId);
+        if (!isCourse) {
+            return res
+                    .status(404)
+                    .json({
+                        message: "Internal Server Error"
+                    })
+                    
+        }
+        
+        // finding if course is purchased
+        const isPurchased = await Purchased.findOne({customer: userId, coursesPurchased: courseId});
+
+        if (!isPurchased) {
+            return res
+                    .status(200)
+                    .json(isCourse);
+        }
+
+        // sending videos 
+        const courseVideos = await Video.findOne({courseId: courseId})
+
+        return res
+                .status(200)
+                .json(response = {
+                    isCourse,
+                    courseVideos,
+                    purchased: true
+                });
+        
+    } catch (err) {
+        console.log(err);
+        return res
+                .status(500)
+                .json({
+                    message: "Internal Server Error"
+                })
+    }
+}
+
+const getAllCourses = async (req, res) => {
+    try {
+        const findCourse = await Test.find({security: false});
+        return res
+                .status(200)
+                .json(findCourse)
+    } catch (err) {
+        console.log(err);
+        return res
+                .status(500)
+                .json({
+                    message: "Internal Server Error"
+                })
+    }
+}
+
+const getCourseRecommendation = async (req, res) => {
+    try {
+        const courseRec = await Test.find({security: false}).limit(5);
+        return res
+                .status(200)
+                .json(courseRec);
+    } catch (err) {
+        console.log(err);
         return res
                 .status(500)
                 .json({
@@ -60,5 +138,7 @@ const deleteTestCourse = async (req, res) => {
 
 module.exports = {
     createTestCourse,
-    deleteTestCourse
+    deleteTestCourse,
+    getSpecificCourse,
+    getAllCourses
 }
--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createTestCourse } = require('../controllers/test_controller');
+const { createTestCourse, getSpecificCourse, deleteTestCourse } = require('../controllers/test_controller');
 const router = express.Router();
 const upload = require('../middlewares/multer_templates_config');
 const { altVerifyToken } = require('../middlewares/verifyToken');
 
+
+router.get("/:id", altVerifyToken, getSpecificCourse);
 router.post("/", altVerifyToken,upload.single('template'), createTestCourse);
+router.delete("/:id", altVerifyToken, deleteTestCourse);
 
 module.exports = router;
